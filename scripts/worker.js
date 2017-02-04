375ac7bcc68d734675ac7051e0d977e1090ca53f
Jason <floresj4@gmail.com>
2017-02-04 12:38:03 -0500

    fixed full page rendering with highlight

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,6 +1,10 @@
 onmessage = function(event) {
 	importScripts('../style/highlight/highlight.pack.js');
-	console.log(event);
-	var result = self.hljs.highlightAuto(event.data);
-	postMessage(result.value);
+
+	var msg = event.data;
+	var result = self.hljs.highlightAuto(msg.textContent);
+	
+	//reset the textContent and send back
+	msg.textContent = result.value;	
+	postMessage(msg);
 }
