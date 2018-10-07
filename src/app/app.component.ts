957165e3283afd8aa9ad397329ef6b35cd959916
Jason <floresj4>
2018-10-07 12:38:11 -0400

    moving prologue posts to blogs.data, adding moment.js, and styling blog display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-floresj4';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
 }
